29542a2dd91bc2e98ff0e4a7ba019b19895aefb2
John H <johnnyhill13@gmail.com>
2020-01-23 12:33:06 -0800

    notifications work on iOS and Android emulator

--- App.js
+++ App.js
@@ -1,114 +1,105 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+    import {
+      SafeAreaView,
+      StyleSheet,
+      ScrollView,
+      View,
+      Text,
+      StatusBar,
+      Button,
+      Alert
+    } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+    import {
+      Header,
+      Colors,
+    } from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+    import NotificationService from './components/NotificationService';
 
-export default App;
+    class App extends Component {
+
+      constructor(props) {
+        super(props);
+            //creating a new instance of the NotificationService 
+            //& passing in the function we want called when the notification happens
+        this.notification = new NotificationService(this.onNotification);
+      }
+
+        //Gets called when the notification comes in
+      onNotification = (notif) => {
+        Alert.alert(notif.title, notif.message);
+      }
+
+        //Permissions to use notifications
+      handlePerm(perms) {
+        Alert.alert("Permissions", JSON.stringify(perms));
+      }
+
+      render(){
+        return (
+          <>
+            <StatusBar barStyle="dark-content" />
+            <SafeAreaView>
+              <ScrollView
+                contentInsetAdjustmentBehavior="automatic"
+                style={styles.scrollView}>
+                <Header />
+                {global.HermesInternal == null ? null : (
+                  <View style={styles.engine}>
+                    <Text style={styles.footer}>Engine: Hermes</Text>
+                  </View>
+                )}
+                <View style={styles.body}>
+                  <Button title={"Local Notification"} onPress={() => { this.notification.localNotification() }} />
+                  <Button title={"Scheduled (30s) Notification"} onPress={() => { this.notification.scheduleNotification() }} />
+                </View>
+              </ScrollView>
+            </SafeAreaView>
+          </>
+        );
+      }
+    }
+
+    const styles = StyleSheet.create({
+      scrollView: {
+        backgroundColor: Colors.lighter,
+      },
+      engine: {
+        position: 'absolute',
+        right: 0,
+      },
+      body: {
+        backgroundColor: Colors.white,
+      },
+      sectionContainer: {
+        marginTop: 32,
+        paddingHorizontal: 24,
+      },
+      sectionTitle: {
+        fontSize: 24,
+        fontWeight: '600',
+        color: Colors.black,
+      },
+      sectionDescription: {
+        marginTop: 8,
+        fontSize: 18,
+        fontWeight: '400',
+        color: Colors.dark,
+      },
+      highlight: {
+        fontWeight: '700',
+      },
+      footer: {
+        color: Colors.dark,
+        fontSize: 12,
+        fontWeight: '600',
+        padding: 4,
+        paddingRight: 12,
+        textAlign: 'right',
+      },
+    });
+
+    export default App;
